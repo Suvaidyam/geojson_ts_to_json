0aef032de6cd7848ea82875614774f68a72a9d3e
amreshyadavgit <ami92.raj@gmail.com>
2025-04-06 18:36:35 +0530

    initial commit

---
+++ parse.ts
@@ -0,0 +1,67 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
+
+// Get current file path and directory in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const inputDir = path.resolve(__dirname, './boundries');
+const outputDir = path.resolve(__dirname, 'jsondata');
+
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+}
+
+function getAllTsFiles(dir: string): string[] {
+    const files: string[] = [];
+
+    const items = fs.readdirSync(dir);
+    for (const item of items) {
+        const fullPath = path.join(dir, item);
+        const stat = fs.statSync(fullPath);
+
+        if (stat.isDirectory()) {
+            files.push(...getAllTsFiles(fullPath));
+        } else if (item.endsWith('.ts')) {
+            files.push(fullPath);
+        }
+    }
+
+    return files;
+}
+
+const files = getAllTsFiles(inputDir);
+
+(async () => {
+    for (const filePath of files) {
+        const relativePath = path.relative(inputDir, filePath);
+        const name = path.basename(relativePath, '.ts');
+        const outputPath = path.join(outputDir, path.dirname(relativePath));
+
+        try {
+            // Create nested output directories if they don't exist
+            fs.mkdirSync(outputPath, { recursive: true });
+
+            const mod = await import(filePath);
+            const data = mod.default;
+            if (data?.features) {
+                for (let feature of data?.features) {
+                    if (feature.properties) {
+                        if (feature.properties.st_code && !feature.properties.st_code?.startsWith('S')) {
+                            feature.properties['st_code'] = `S${feature.properties.st_code}`;
+                        }
+                        if (feature.properties.dt_code && !feature.properties.dt_code?.startsWith(feature.properties.st_code)) {
+                            feature.properties['dt_code'] = `${feature.properties.st_code}${feature.properties.dt_code}`;
+                        }
+                    }
+                }
+            }
+            fs.writeFileSync(path.join(outputPath, `${name}.json`), JSON.stringify(data, null, 2));
+            console.log(`✅ ${relativePath} -> ${name}.json written.`);
+        } catch (err) {
+            console.error(`❌ Failed to process ${relativePath}:`, err);
+        }
+    }
+})();
